25a4aa4412ed290a091cc3ebf894a5227fec6453
Manav <khatiwadamanav123@gmail.com>
2026-07-31 12:14:59 +0000

    [+]docker-compose.yml , compose file in working condition

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.1.96:5500"],
+    allow_origins=[
+        "http://192.168.1.96",
+        "http://192.168.1.96:80",
+        "http://192.168.1.96:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,10 +24,22 @@ app.add_middleware(
 
 app.include_router(images.router)
 
+
 @app.on_event("startup")
 def startup():
-	print("Connecting to PostgreSQL...")
-	test_connection()
+    print("Connecting to PostgreSQL...")
+
+    for attempt in range(10):
+        try:
+            test_connection()
+            print("Connected!")
+            return
+        except OperationalError:
+            print(f"Attempt {attempt + 1}/10 failed...")
+            time.sleep(2)
+
+    raise Exception("Could not connect to PostgreSQL.")
+
 
 @app.get("/")
 def root():
